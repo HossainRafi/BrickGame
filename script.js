53d465b42e9d7ff9ba49c804a9168f33e847eac2
HossainRafi <hossainrafi71@gmail.com>
2022-12-04 18:56:14 +0600

    player drop function

--- script.js
+++ script.js
@@ -125,3 +125,16 @@ function rotate(matrix, dir) {
     matrix.reverse();
   }
 }
+
+function playerDrop() {
+  player.pos.y++;
+  if (collide(arena, player)) {
+    player.pos.y--;
+    merge(arena, player);
+    playerReset();
+    arenaSweep();
+    updateScore();
+  }
+  dropCounter = 0;
+}
+
