2f2e776456c93c234a21146b46fe265f55fd706b
HossainRafi <hossainrafi71@gmail.com>
2022-12-04 20:00:03 +0600

    project done

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const colors = [
   null,
   "#FF0D72",
   "#0DC2FF",
+  "#FF8E0D",
   "#0DFF72",
   "#F538FF",
-  "#FF8E0D",
   "#FFE138",
   "#3877FF",
 ];
@@ -215,3 +215,12 @@ document.addEventListener("keydown", (event) => {
   }
 });
 
+const arena = createMatrix(12, 20);
+const player = {
+  pos: { x: 0, y: 0 },
+  matrix: null,
+  score: 0,
+};
+playerReset();
+updateScore();
+update();
