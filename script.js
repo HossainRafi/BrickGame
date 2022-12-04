4f602736abe9d277ccc83df594f24735fe918b8e
HossainRafi <hossainrafi71@gmail.com>
2022-12-04 14:12:50 +0600

    outer arena

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 const canvas = document.getElementById("tetris");
 const context = canvas.getContext("2d");
 context.scale(20, 20);
+
+function arenaSweep() {
+  let rowCount = 1;
+  outer: for (let y = arena.length - 1; y > 0; --y) {
+    for (let x = 0; x < arena[y].length; ++x) {
+      if (arena[y][x] === 0) {
+        continue outer;
+      }
+    }
+  }
+} 
